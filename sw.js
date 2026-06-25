28b514d1fb04efd5011197f2bd5c3af575d9fc26
ymouse91 <jouko.riikonen@live.fi>
2026-06-25 14:45:50 +0300

    number entry

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "turing-challenge-v2";
+const CACHE_NAME = "turing-challenge-v3";
 const ASSETS = [
   "./",
   "./index.html",
-  "./styles.css?v=2",
-  "./app.js?v=2",
+  "./styles.css?v=3",
+  "./app.js?v=3",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
